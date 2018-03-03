7de0529815c6a3d377c5d0db44bbf0ca0109782c
Derek Chen <derekchen14@gmail.com>
2018-03-02 17:40:57 -0800

    Update batchers to handle gpu

--- persona-chat/neural_model/batcher.py
+++ persona-chat/neural_model/batcher.py
@@ -2,7 +2,8 @@ import numpy as np
 import random
 from itertools import izip_longest, izip
 from cocoa.core.util import read_pickle, write_pickle
-from cocoa.pt_model.util import smart_variable
+from torch import LongTensor
+from torch.autograd import Variable
 
 class DialogueBatcher(object):
     def __init__(self, vocab, split_type, shuffle=True):
@@ -17,11 +18,11 @@ class DialogueBatcher(object):
         for example in raw_data:
             source_tokens = example[0]
             source_indexes = [self.vocab.word_to_ind[st] for st in source_tokens]
-            source = smart_variable(source_indexes, dtype="list")
+            source = Variable(LongTensor(source_indexes))
 
             target_tokens = example[1]
             target_indexes = [self.vocab.word_to_ind[tt] for tt in target_tokens]
-            target = smart_variable(target_indexes, dtype="list")
+            target = Variable(LongTensor(target_indexes))
 
             data.append((source, target))
         return data
